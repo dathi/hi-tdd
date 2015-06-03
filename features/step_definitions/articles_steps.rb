5efa9ff44415c1d8157ecf3adf2e513558057a00
dathi <dathi@tma.com.vn>
2015-06-03 11:06:06 +0700

    implement Given and When in steps def

--- features/step_definitions/articles_steps.rb
+++ features/step_definitions/articles_steps.rb
@@ -1,15 +1,16 @@
 Given(/^some published articles$/) do
-  pending # express the regexp above with the code you wish you had
+  FactoryGirl.create_list :published_articles, 3
 end
 
 Given(/^some unpublished articles$/) do
-  pending # express the regexp above with the code you wish you had
+  FactoryGirl.create_list :unpublished_articles, 3
 end
 
 When(/^I ask for articles from the API$/) do
-  pending # express the regexp above with the code you wish you had
+  header "Accept", "application/json"
+  get "api/v1/articles"
 end
 
 Then(/^I should only receive published articles as JSON$/) do
-  pending # express the regexp above with the code you wish you had
+	
 end
