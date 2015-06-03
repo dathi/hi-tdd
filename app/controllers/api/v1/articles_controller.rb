52e464b8a9ed00e213bfbba6c916f2b853637650
dathi <dathi@tma.com.vn>
2015-06-03 13:14:55 +0700

    add failing spec for getting list published article in model

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
+  	@articles = Article.published
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".published" do
+  	before do
+  	  FactoryGirl.create_list :published_article, 2
+  	  FactoryGirl.create_list :unpublished_article, 1
+
+  	  @published = Article.published
+  	end
+
+  	it "only return published articles" do
+  	  @published.should_not be_empty
+  	  @published.each do |article|
+  	  	article.should be_published
+  	  end
+  	end
+  end
 end
