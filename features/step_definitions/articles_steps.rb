7be3c24c8cea34fd6fc1f7c9c5a5b138d2e8e54d
dathi <dathi@tma.com.vn>
2015-06-03 10:56:40 +0700

    add feature file and steps def file

---
+++ features/step_definitions/articles_steps.rb
@@ -0,0 +1,15 @@
+Given(/^some published articles$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^some unpublished articles$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I ask for articles from the API$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should only receive published articles as JSON$/) do
+  pending # express the regexp above with the code you wish you had
+end
