12dfde95cd409d4379915b42b7bd02cbd613affd
dathi <dathi@tma.com.vn>
2015-06-03 13:55:39 +0700

    stop here, pass spec, but still fail for cucumber, will back if i have chance

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  describe ".published" do
-  	before do
-  	  FactoryGirl.create_list :published_article, 2
-  	  FactoryGirl.create_list :unpublished_article, 1
-
-  	  @published = Article.published
-  	end
-
-  	it "only return published articles" do
-  	  expect(@published).not_to be_empty
-  	  @published.each do |article|
-  	  	expect(article).to be_published
-  	  end
-  	end
+  describe '.published' do
+    before do
+      FactoryGirl.create_list(:published_article, 2)
+      FactoryGirl.create_list(:unpublished_article, 1)
+ 
+      @published = Article.published
+    end
+ 
+    it 'only returns published articles' do
+      expect(@published).not_to be_empty
+      @published.each do |article|
+        expect(article).to be_published
+      end
+    end
   end
 end
+
